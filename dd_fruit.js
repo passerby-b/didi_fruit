20bfe1b4a017122aa5dfdea9ed7dcaeb27a03d38
passerby-b <40885345+passerby-b@users.noreply.github.com>
2021-05-20 13:46:37 +0800

    Update dd_fruit.js

--- dd_fruit.js
+++ dd_fruit.js
@@ -3,7 +3,7 @@ v.3.1
 滴滴橙心果园脚本,支持自动浇水及部分任务
 手动抓包获取token,手机在boxjs里填写
 boxjs订阅地址:https://gitee.com/passerby-b/javascript/raw/master/JD/passerby-b.boxjs.json
-docker环境变量名:DD_TOKEN,多账号换行或英文逗号(,)隔开
+docker环境变量名:DD_TOKEN,多账号换行或英文逗号(,)隔开 
 
 [task_local]
 10 0,8,12,18 * * * https://raw.githubusercontent.com/passerby-b/didi_fruit/main/dd_fruit.js

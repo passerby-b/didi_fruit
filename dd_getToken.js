db1bc72e8abc7a5f316d6df9f9989ec85c4fd4fd
passerby-b <40885345+passerby-b@users.noreply.github.com>
2021-05-20 16:00:10 +0800

    Update dd_getToken.js

--- dd_getToken.js
+++ dd_getToken.js
@@ -1,6 +1,7 @@
 /*
-滴滴获取Cookie脚本
+滴滴获取token脚本
 重写地址:game.xiaojukeji.com/api/game/plant/watering,触发类型:request-body
+进果园浇一次水
 MITM添加:game.xiaojukeji.com
 每次获取会覆盖之前的ck,请先用boxjs备份
 多账号可以每次获取后复制出来最后拼在一起用英文逗号(,)分隔后一起复制到boxjs里
